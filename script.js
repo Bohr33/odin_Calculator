1689502e48747af0337c670897d9c1e5da39d5b1
Ben Ward <brw775586@gmail.com>
2022-06-07 00:47:33 -0400

    Updated some of the functionality of the calculator, the logic is working better but still need to fix some bugs

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ function addClickEventListener(list){
         if(i == 9){
             list[i].addEventListener('click', function(){digitsFun(0)});    
         }else if(i == 10){
-            list[i].addEventListener('click', function(){digitsFun('.')});
+            list[i].addEventListener('click', function(){digitsFunDot('.')});
         }else{
             list[i].addEventListener('click', function(){digitsFun(this.textContent)});
         };
     }
 };
+
+function addClickEventFunctions(list){
+    list[0].addEventListener('click', function(){nextValue(storeDigitFront, "add")});
+    list[1].addEventListener('click', function(){nextValue(storeDigitFront, "subtract")});
+    list[2].addEventListener('click', function(){nextValue(storeDigitFront, "multiply")});
+    list[3].addEventListener('click', function(){nextValue(storeDigitFront, "divide")});
+    list[4].addEventListener('click', function(){clear()});
+    list[5].addEventListener('click', function(){operate(storeOperator, parseInt(storeDigitBack), parseInt(storeDigitFront))});
+    
+    
+}
 
 function add(a,b){
     console.log(a + b);
@@ -29,20 +40,28 @@ function divide(a,b){
 };
 
 function operate(operator, a, b){
+
+
+    let output;
     switch(operator){
         case 'add':
-            return add(a,b);
+            output =  add(a,b);
             break;
         case 'subtract':
-            return subtract(a,b);
+            output =  subtract(a,b);
             break;
         case 'multiply':
-            return multiply(a,b);
+            output =  multiply(a,b);
             break;
         case 'divide':
-            return divide(a,b);
+            output =  divide(a,b);
     };
+    storeOperator = operator;
+    storeDigitBack = output;
+    storeDigitFront = "";
+    updateDisplay(output);
 };
+
 let displayVar;
 let display = document.querySelector('#display');
 // display.textContent = "Working!"
@@ -51,20 +70,48 @@ function updateDisplay(a){
     display.textContent = a;
 }
 
-let digitStore = "";
+//stores value in display
+let storeDigitFront = "";
+//digits Node list
 let digits = document.querySelectorAll('.digits');
-console.log(digits);
+//adds listeners to digit buttons
 addClickEventListener(digits);
+
+//functions node list
+let functions = document.querySelectorAll('.functions');
+addClickEventFunctions(functions);
 
 
 function digitsFun(digit){
-    digitStore += digit;
-    console.log(digitStore);
-    updateDisplay(digitStore)
+    storeDigitFront += digit;
+    updateDisplay(storeDigitFront)
 }
 
-function test(a){
-    console.log(a);
+//this variables stores whether a decimal is already in the storeDigitFront variable
+let dotwatch = false;
+function digitsFunDot(dot){
+    if(dotwatch == false){
+        storeDigitFront += dot;
+        dotwatch = true;
+        updateDisplay(storeDigitFront);
+    };
+}
+
+let storeDigitBack = 0;
+let storeOperator;
+
+//stores the chosen number in a value, resets the storeDigitFront value, and sets the operator function to be sent for operation
+function nextValue(digit, operator){
+    storeDigitBack = digit;
+    storeDigitFront = "";
+    storeOperator = operator;
+    console.log(storeDigitBack);
+}
+
+function clear(){
+    storeDigitFront = "";
+    storeDigitBack = 0;
+    updateDisplay(0);
 }
 
 
